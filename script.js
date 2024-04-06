837ecc56f6ee689ae42ad1e8593dd5d583c9587c
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-04-06 20:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input = document.getElementById("fname");
+
+input.addEventListener('keyup',function Capitalized(e) {
+	   let copy = e.target
+	     copy.value = copy.value.toUpperCase();
+	
+})

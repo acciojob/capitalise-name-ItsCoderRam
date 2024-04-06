831ba4f70c91f52333f4810ef6c6a6a738915a4b
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-04-06 20:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let input = document.getElementById("fname");
 
-input.addEventListener('keyup',function Capitalized(e) {
+input.addEventListener('blur',function Capitalized(e) {
 	   let copy = e.target
 	     copy.value = copy.value.toUpperCase();
 	
